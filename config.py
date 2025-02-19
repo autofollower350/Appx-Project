4be312b7172348033b74cf5bc2ef570cca068713
autofollower350 <followerauto350@gmail.com>
2025-02-19 09:48:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ nest_asyncio.apply()
 # 🔹 Telegram Bot API Keys (अपनी API डिटेल्स डालें)
 API_ID = "28590286"  # अपना सही API_ID डालें
 API_HASH = "6a68cc6b41219dc57b7a52914032f92f"  # अपना सही API_HASH डालें
-BOT_TOKEN = "7412939071:AAFk6_MJziu22UnZOv9crH-HcwLDqzQ1SVg"  # ✅ अपना सही BOT_TOKEN डालें
+BOT_TOKEN = "7949674880:AAF6FtDotEnZ5m1NEK0cE7YKNHK_tisd8Mk"  # ✅ अपना सही BOT_TOKEN डालें
